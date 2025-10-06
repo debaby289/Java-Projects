d1b678d9c87488183337bfcd25fc72d6702af7f4
Devine <simsdevine@gmail.com>
2025-10-06 18:03:04 -0500

    25.10.06 Personalization
    - Added comments
    - Adjusted html, css, and js

--- Expanding_Cards/script.js
+++ Expanding_Cards/script.js
@@ -1,5 +1,7 @@
+// Select all elements with the class 'panel'
 const panels = document.querySelectorAll('.panel')
 
+// Add click event listener to each panel
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
         removeActiveClasses()
@@ -7,6 +9,7 @@ panels.forEach(panel => {
     })
 })
 
+// Function to remove 'active' class from all panels
 function removeActiveClasses() {
     panels.forEach(panel => {
         panel.classList.remove('active')
