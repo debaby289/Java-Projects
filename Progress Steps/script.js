0632fa09f1afdb96bdeaaa06be0e8d78b584682b
Devine <simsdevine@gmail.com>
2025-10-07 12:11:02 -0500

    25.10.07 Personalization

--- Progress Steps/script.js
+++ Progress Steps/script.js
@@ -1,13 +1,17 @@
+// Progress Steps
 const progress = document.getElementById('progress');
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
 const circles = document.querySelectorAll('.circle');
 
+// Current active step
 let currentActive = 1;
 
+// Event listeners for next and previous buttons
 next.addEventListener('click',() => {
     currentActive++
 
+    // Ensure currentActive does not exceed number of circles
     if(currentActive > circles.length){
         currentActive = circles.length
     }
@@ -15,6 +19,7 @@ next.addEventListener('click',() => {
     update()
 })
 
+// Event listener for previous button
 prev.addEventListener('click',() => {
     currentActive--
     if(currentActive < 1){
@@ -24,6 +29,7 @@ prev.addEventListener('click',() => {
     update()
 })
 
+// Update the progress bar and circles based on current active step
 function update(){
     circles.forEach((circle, idx) => {
         if(idx < currentActive){
@@ -33,10 +39,13 @@ function update(){
         }
     })
 
+    // Update the progress bar width
     const actives = document.querySelectorAll('.active')
 
+    // Calculate the width percentage based on active circles
     progress.style.width = (actives.length -1) / (circles.length -1) * 100 + '%'
 
+    // Enable/disable buttons based on current active step
     if(currentActive === 1) {
         prev.disabled = true
     } else if (currentActive === circles.length){
